3db0800c18b63c30c33da118e8834e54bd548b16
kimhongchan2004-a11y <kimhongchan2004@gmail.com>
2026-09-14 12:28:10 +0900

    kotlin basics v0.3 (function)

--- KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -28,6 +28,7 @@ class MainActivity : ComponentActivity() {
             }
         }
         week03Variables()
+        week03Functions()
     }
 }
 
@@ -61,6 +62,32 @@ private fun week03Variables() {
     nickname = "mirae"
     println("Nickname: $nickname ${nickname?.length}")
 }
+
+private fun week03Functions(){
+//    println("Week 03: Functions")
+//
+//    fun greet(name: String) = "Hello, $name!"
+//
+//    println(greet("Android developer"))
+
+    println("== Kotlin Functions ==")
+
+    fun greet(name: String): String {
+        return "Hello, $name!"
+    }
+
+    fun add(a: Int, b: Int) = a + b
+
+    fun introduce(name: String, age: Int = 19){
+        println("My name is $name and I'm $age years old")
+    }
+
+    println(greet("Kotlin"))
+    println("Sum: ${add(5, -71)}")
+    introduce("Kim", 7)
+    introduce("Park")
+}
+
 
 @Composable
 fun Greeting(name: String, modifier: Modifier = Modifier) {
