a0362ec0e60502699b7d46aa87668052701e8efc
kimhongchan2004-a11y <kimhongchan2004@gmail.com>
2026-09-21 12:37:08 +0900

    Update Kotlin and Android project files

--- KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
+++ KotlinBasics/app/src/main/java/com/kotlinbasics/MainActivity.kt
@@ -2,6 +2,7 @@ package com.kotlinbasics
 
 import android.os.Bundle
 import android.system.StructMsghdr
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
@@ -56,7 +57,36 @@ private fun week04Collections() {
 
     scores.forEach{(name, score) -> println("$name scored $score")}
     fruits.forEach{fruit -> println("$fruit")}
+    Log.d("KotlinWeek03", "== Kotlin Classes ==")
+
+    class Person(val name: String, var age: Int){
+        fun introduce(){
+            Log.d("KotlinWeek03", "안녕하세요, $name ($age 세)입니다.")
+        }
+        fun birthday(){
+            age++
+            Log.d("KotlinWeek03", "$name 의 생일! 이제 $age 세...")
+        }
+    }
+    val person1 = Person("홍길동", 27)
+    person1.introduce()
+    person1.birthday()
+
+    class Animal(var species: String){
+        var weight: Double = 0.0
+        constructor(species: String, weight: Double) : this(species){
+            this.weight = weight
+            Log.d("KotlinWeek03", "$species 의 무게 : $weight kg")
+        }
+        fun makeSound(){
+            Log.d("KotlinWeek03", "$species 가 소리를 냅니다.")
+        }
+    }
+    val puppy = Animal("웰시코기", 10.5)
+    puppy.makeSound()
 }
+
+
 
 private fun week04Classes() {
     println(" ==kotlin Classes == ")
